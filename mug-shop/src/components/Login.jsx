bf37622eadcba4077ce02b9377c23128df14eda5
ahadmirhabibi <ahad.mirhabibi@gmail.com>
2023-05-01 01:00:23 +0430

    unstable-version

--- mug-shop/src/components/Login.jsx
+++ mug-shop/src/components/Login.jsx
@@ -1,28 +1,49 @@
 import React, {useEffect, useState} from "react";
 import '../assets/styles/Login.css';
-import { ImMug } from "react-icons/im";
+import {ImMug} from "react-icons/im";
 import {useParams} from "react-router-dom";
 import axios from "axios";
 
-function Login(){
+function Login() {
     const section = useParams();
-    const [pageState ,setPageState] =useState(section.section);
-    const [confirmMode ,setConfirmMode] =useState('register');
-    const call_register = ()=>{
-        axios.get('http://localhost:8000/api/user/register').then(
-            (response)=>{
-                console.log(response['data']['message'])
-            }
-        )
+    const [pageState, setPageState] = useState(section.section);
+    const [confirmMode, setConfirmMode] = useState('register');
+    const [registerEmail, setRegisterEmail] = useState('ahad7.mirhabibi@gmail.com');
+    const [registerPassword, setRegisterPassword] = useState('2345678');
+    const [registerPasswordConfirm, setRegisterPasswordConfirm] = useState('12345678');
+
+
+    const call_register = (e) => {
+        e.preventDefault();
+        var bodyFormData = new FormData();
+        bodyFormData.append('email',registerEmail);
+        bodyFormData.append('password',registerPassword);
+        axios({
+            method: "post",
+            url: "http://localhost:8000/api/user/register",
+            data: bodyFormData,
+            headers: { "Content-Type": "multipart/form-data" },
+        })
+            .then(function (response) {
+                //handle success
+                console.log(response);
+            })
+            .catch(function (response) {
+                console.log(response)
+                let status = parseInt(response['response']['status']);
+                const errors = response['response']['data']['errors'];
+                console.log(errors)
+
+            });
+
     }
-    useEffect((
-    )=>{
+    useEffect(() => {
         setPageState(section.section)
-    } ,[section] )
-    return(
+    }, [section])
+    return (
         <div className={'login-container hero'}>
             {
-                pageState ==='login' &&
+                pageState === 'login' &&
                 <form className={'login-form'}>
                     <div className="login-top">
                         <span className={'hero title'}>ورود به حساب کاربری</span>
@@ -30,21 +51,22 @@ function Login(){
                     <div className="login-mid">
                         <div className="login-row">
                             <span>ایمیل</span>
-                            <input type={"text"} placeholder={'ایمیل خود را وارد کنید'}/>
+                            <input name={'email'} type={"text"} placeholder={'ایمیل خود را وارد کنید'}/>
                         </div>
                         <div className="login-row">
                             <span>رمز عبور</span>
-                            <input className={'dir-left'} type={"password"} placeholder={'رمز عبور خود را وارد کنید'}/>
+                            <input name={'password'} className={'dir-left'} type={"password"}
+                                   placeholder={'رمز عبور خود را وارد کنید'}/>
                         </div>
 
                     </div>
                     <div className="login-action">
-                        <button>ورود</button>
+                        <button type={"submit"}>ورود</button>
                     </div>
-                    <span onClick={()=>setPageState('forget')} className={'forget-link'}>
+                    <span onClick={() => setPageState('forget')} className={'forget-link'}>
                     فراموشی رمز عبور
                 </span>
-                    <span onClick={()=>setPageState('register')} className={'register-link'}>
+                    <span onClick={() => setPageState('register')} className={'register-link'}>
                    ثبت نام در ماگ شاپ
                 </span>
                 </form>
@@ -63,17 +85,20 @@ function Login(){
 
                     </div>
                     <div className="forget-action">
-                        <button onClick={()=>{
+                        <button onClick={() => {
                             setConfirmMode('forget');
-                            setPageState('confirm');}}>باز نشانی رمز عبور</button>
+                            setPageState('confirm');
+                        }}>باز نشانی رمز عبور
+                        </button>
                     </div>
                 </form>
             }
             {
-                pageState ==='confirm' &&
+                pageState === 'confirm' &&
                 <form className={'confirm-form'}>
                     <div className="confirm-top">
-                        <span className={'title'}>{confirmMode==='register'?'تکمیل ثبت نام':'بازنشانی رمز عبور'}</span>
+                        <span
+                            className={'title'}>{confirmMode === 'register' ? 'تکمیل ثبت نام' : 'بازنشانی رمز عبور'}</span>
                     </div>
                     <div className="confirm-mid">
                         <div className="confirm-row">
@@ -89,51 +114,56 @@ function Login(){
             }
             {
                 pageState === 'register' &&
-                <form className={'register-form'}>
+                <form className={'register-form'} onSubmit={call_register} >
                     <div className="register-top">
                         <span className={'title'}>ثبت نام در ماگ شاپ</span>
                     </div>
                     <div className="register-mid">
                         <div className="register-row">
                             <span>ایمیل</span>
-                            <input type={"text"} placeholder={'ایمیل خود را وارد کنید'}/>
+                            <input name={'email'} type={"text"} placeholder={'ایمیل خود را وارد کنید'}
+                                   />
                         </div>
                         <div className="register-row">
                             <span>رمز عبور</span>
-                            <input className={'dir-left'} type={"password"} placeholder={'رمز عبور خود را وارد کنید'}/>
+                            <input name={'password'} className={'dir-left'} type={"password"}
+                                   placeholder={'رمز عبور خود را وارد کنید'}
+                                  />
                         </div>
                         <div className="register-row">
                             <span>تکرار رمز عبور</span>
-                            <input className={'dir-left'} type={"password"} placeholder={'تکرار رمز عبور خود را وارد کنید'}/>
+                            <input className={'dir-left'} type={"password"}
+                                   placeholder={'تکرار رمز عبور خود را وارد کنید'}/>
                         </div>
 
                     </div>
                     <div className="register-action">
-                        <button onClick={()=>{
-
-                            setConfirmMode('register');
-                            setPageState('confirm');
-                            call_register();
-                        }
-                        }
-                        >ثبت نام</button>
+                        <button type="submit"
+                        //         onClick={() => {
+                        //     setConfirmMode('register');
+                        //     setPageState('confirm');
+                        // }
+                        // }
+                        >ثبت نام
+                        </button>
                     </div>
-                    <span onClick={()=>setPageState('login')} className={'register-link'}>
+                    <span onClick={() => setPageState('login')} className={'register-link'}>
                    ورود به ماگ شاپ
                 </span>
                 </form>
 
             }
 
-        < ImMug className={'cube'}/>
-        < ImMug className={'cube'}/>
-        < ImMug className={'cube'}/>
-        < ImMug className={'cube'}/>
-        < ImMug className={'cube'}/>
-        < ImMug className={'cube'}/>
+            < ImMug className={'cube'}/>
+            < ImMug className={'cube'}/>
+            < ImMug className={'cube'}/>
+            < ImMug className={'cube'}/>
+            < ImMug className={'cube'}/>
+            < ImMug className={'cube'}/>
 
         </div>
 
     )
 }
+
 export default Login;
