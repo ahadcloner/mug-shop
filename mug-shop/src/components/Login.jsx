7763da504215592dcf4f1b961174dd75b5ab6047
ahadmirhabibi <ahad.mirhabibi@gmail.com>
2023-05-18 22:49:03 +0430

    handle user activate account

--- mug-shop/src/components/Login.jsx
+++ mug-shop/src/components/Login.jsx
@@ -7,7 +7,7 @@ import {useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 import {useCookies} from "react-cookie";
 import {Notifier} from "./Utils/Notifier";
-
+import {Simple_get} from "./Utils/RequstSender";
 
 
 function Login() {
@@ -21,39 +21,43 @@ function Login() {
     const [loginEmail, setLoginEmail] = useState('');
     const [loginPassword, setLoginPassword] = useState('');
     const navigate = useNavigate();
+    const [confirmCode, setConfirmCode] = useState('');
 
-    // const notify_success = (message) => toast.success(message, {
-    //     position: "top-center",
-    //     autoClose: 5000,
-    //     hideProgressBar: false,
-    //     closeOnClick: true,
-    //     pauseOnHover: true,
-    //     draggable: true,
-    //     progress: undefined,
-    //     theme: "colored",
-    // });
-    // const notify_danger = (message) => toast.error(message, {
-    //     position: "top-center",
-    //     autoClose: 5000,
-    //     hideProgressBar: false,
-    //     closeOnClick: true,
-    //     pauseOnHover: true,
-    //     draggable: true,
-    //     progress: undefined,
-    //     theme: "colored",
-    // });
-    useEffect(()=>{
+    useEffect(() => {
 
-        if(cookie.token ){
-            navigate('/')
+        if (cookie.token) {
+            // navigate('/')
+            check_activate_account(cookie.token).then();
         }
-    },[cookie.token])
+    }, [cookie.token])
+
+
+    const check_activate_account = async (mycookie) => {
+
+        const data = await Simple_get('https://hitmug.ir/api/user/auth/status', true, '', mycookie, 'get', [])
+            .then((d) => {
+                if (parseInt(d?.[2]) >= 200 && parseInt(d?.[2]) < 300) {
+                    if (parseInt(d?.[0]) === 0) {
+                        send_verification_mail();
+                        setConfirmMode('register');
+                        setPageState('confirm');
+                    } else if (parseInt(d?.[0]) === 1) {
+                        navigate('/');
+                    }
+
+                } else {
+                    if (d[0].response.data.errors?.email) Notifier('danger', d[0].response.data.errors.email[0])
+                    if (d[0].response.data.errors?.password) Notifier('danger', d[0].response.data.errors.password[0])
+                }
+            })
+    }
     const call_register = async (e) => {
         e.preventDefault();
         if (registerPassword === registerPasswordConfirm) {
             var bodyFormData = new FormData();
             bodyFormData.append('email', registerEmail);
             bodyFormData.append('password', registerPassword);
+
             let api = await axios({
                 method: "post",
                 url: "https://hitmug.ir/api/user/register",
@@ -61,62 +65,78 @@ function Login() {
                 headers: {"Content-Type": "multipart/form-data"},
             })
                 .then(function (response) {
-                    Notifier('success',response['data']['message'])
+                    Notifier('success', response['data']['message'])
                     setConfirmMode('register');
-                    setPageState('confirm');
+                    setPageState('login');
                 })
                 .catch(function (response) {
                     let status = parseInt(response['response']['status']);
                     const errors = response['response']['data']['errors'];
-                    Notifier('danger',errors['email'][0])
-                    Notifier('danger',errors['password'][0])
+                    Notifier('danger', errors['email'][0])
+                    Notifier('danger', errors['password'][0])
                 });
         } else {
-            Notifier('danger','رمز عبور با تکرار رمز عبور مطابقت ندارد')
+            Notifier('danger', 'رمز عبور با تکرار رمز عبور مطابقت ندارد')
         }
 
 
     }
     const call_login = async (e) => {
         e.preventDefault();
-
-
-
-
-
         const data = new FormData();
         data.append('email', loginEmail);
         data.append('password', loginPassword);
+
         let api = await axios.post('https://hitmug.ir/api/user/login', data)
             .then((response) => {
-                Notifier('success',response['data']['message']);
+                Notifier('success', response['data']['message']);
                 let token = response['data']['token'];
-                console.log(token)
-                setCookie('token' , token);
+                setCookie('token', token);
             })
             .catch((response) => {
                 // console.log(response);
                 const errors = response['response']['data']['errors'];
 
 
-                    try {
-                        Notifier('danger',errors['password'][0])
-                    }catch (e){}
-                    try {
-                        Notifier('danger',errors['email'][0])
-                    }catch (e){}
-                    try {
-                        Notifier('danger',response['response']['data']['message'])
-                    }
-                    catch (e){}
-
-
+                try {
+                    Notifier('danger', errors['password'][0])
+                } catch (e) {
+                }
+                try {
+                    Notifier('danger', errors['email'][0])
+                } catch (e) {
+                }
+                try {
+                    Notifier('danger', response['response']['data']['message'])
+                } catch (e) {
+                }
 
 
             })
 
     }
 
+    const send_verification_mail = async () => {
+        const data = await Simple_get('https://hitmug.ir/api/user/auth/verify-mail', true, '', cookie.token, 'post', [])
+            .then((d) => {
+                if (parseInt(d?.[2]) >= 200 && parseInt(d?.[2]) < 300) {
+                    console.log('sended');
+                }
+            });
+    }
+    const verify_mail = async (e) => {
+        e.preventDefault()
+        console.log('i am verify activate acocount')
+        const data =await Simple_get('https://hitmug.ir/api/user/auth/activate-account/', true, confirmCode, cookie.token, 'post', [])
+            .then((d) => {
+                if (parseInt(d?.[2]) >= 200 && parseInt(d?.[2]) < 300) {
+                    Notifier('success', 'حساب کاربری شما با موفقیت فعال شد');
+                    navigate('/');
+                } else {
+                    Notifier('danger', 'خطا در فعال سازی حساب کاربری')
+                }
+            })
+    }
 
     useEffect(() => {
         setPageState(section.section)
@@ -194,12 +214,14 @@ function Login() {
                     <div className="confirm-mid">
                         <div className="confirm-row">
                             <span>کد چهار رقمی</span>
-                            <input type={"text"} placeholder={'کد چهار رقمی ارسال شده به ایمیلتان را وارد کنید'}/>
+                            <input value={confirmCode} onChange={(e)=>setConfirmCode(e.target.value)}
+                                   type={"text"} placeholder={'کد چهار رقمی ارسال شده به ایمیلتان را وارد کنید'}/>
                         </div>
 
                     </div>
                     <div className="confirm-action">
-                        <button>ارسال</button>
+                        <button type={"submit"} onClick={confirmMode==='register'?(e)=>verify_mail(e):()=>{}}>ارسال
+                        </button>
                     </div>
                 </form>
             }
