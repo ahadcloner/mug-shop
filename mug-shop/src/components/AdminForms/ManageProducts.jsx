72d2d49062de3d86255604ac78d875cccb109e8a
ahadmirhabibi <ahad.mirhabibi@gmail.com>
2023-05-22 15:01:18 +0330

    unstable version

---
+++ mug-shop/src/components/AdminForms/ManageProducts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './ManageProducts.css';
+
+function ManageProducts()
+{
+    return (
+        <div className={'pm-container'}>
+            <div className={'pm-top'}>
+                <h2>مدیریت محصولات سایت</h2>
+            </div>
+        </div>
+    )
+}
+export default ManageProducts;
