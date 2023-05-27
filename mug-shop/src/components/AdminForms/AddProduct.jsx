6c54193221170388444c1b0c524bb034a0cbf8e9
ahadmirhabibi <ahad.mirhabibi@gmail.com>
2023-05-27 17:22:10 +0330

    searchable select box added

--- mug-shop/src/components/AdminForms/AddProduct.jsx
+++ mug-shop/src/components/AdminForms/AddProduct.jsx
@@ -2,16 +2,151 @@ import React, {useEffect, useState} from "react";
 import './AddProduct.css';
 import ImageUploader from "./ImageUploader";
 import p1 from "../../assets/images/1.jpg";
-import SelectSearch from "react-select-search";
-import 'react-select-search/style.css'
+import SelectBox from "../Utils/select-box/SelectBox";
+import {Simple_get} from "../Utils/RequstSender";
+import {useCookies} from "react-cookie";
+import {Notifier} from "../Utils/Notifier";
+import Creatable, { useCreatable } from 'react-select/creatable'
+import {CreatableSelect_multi,CreatableSelect_single} from "../Utils/select-box/SelectBox";
+// import CreatableSelect from 'react-select/creatable';
 
-function AddProduct()
-{
+function AddProduct() {
     let data = [
         {id: 1, title: 'ماگ حرارتی مدل اول', price: '27,000', picture: p1},
     ];
-    const [selectedBanner ,setSelectedBanner] = useState();
-    return(
+    const [cookie, setCookie, removeCookie] = useCookies(['token']);
+    const [selectedBanner, setSelectedBanner] = useState();
+    const [categories, setCategories] = useState([]);
+    const [refreshCategories , setRefreshCategories] = useState(false);
+    const [brands , setBrands] = useState();
+    const [tags , setTags] = useState();
+    const[ refreshBrands ,setRefreshBrands] = useState(false)
+    const[ refreshTags ,setRefreshTags] = useState(false)
+    const chanage_refresh_brands = ()=>{
+        setRefreshBrands(!refreshBrands);
+    }
+    const chanage_refresh_tags = ()=>{
+        setRefreshTags(!refreshTags);
+    }
+    const change_refresh_categories = ()=>{
+        setRefreshCategories(!refreshCategories);
+    }
+    const get_categories =async () => {
+        const data = await
+            Simple_get('https://hitmug.ir/api/product-category/index',false ,'','' ,'get',[])
+                .then((d)=>{
+                    if(d?.[2]>=200 && d?.[2]<=300)
+                    {
+                        setCategories(d?.[0]);
+                        console.log(categories);
+                    }
+                    else
+                    {
+                        Notifier('danger' ,'خطا در دریافت دسته بندی ها');
+                    }
+                })
+    }
+    const get_brands =async () => {
+        const data = await
+            Simple_get('https://hitmug.ir/api/brand/index',true ,'',cookie.token,'get',[])
+                .then((d)=>{
+                    if(d?.[2]>=200 && d?.[2]<=300)
+                    {
+                        setBrands(d?.[0]);
+                    }
+                    else
+                    {
+                        Notifier('danger' ,'خطا در دریافت برند ها');
+                    }
+                })
+    }
+    const get_tags =async () => {
+        const data = await
+            Simple_get('https://hitmug.ir/api/tag/index',true ,'',cookie.token,'get',[])
+                .then((d)=>{
+                    if(d?.[2]>=200 && d?.[2]<=300)
+                    {
+                        setTags(d?.[0]);
+                    }
+                    else
+                    {
+                        Notifier('danger' ,'خطا در دریافت تگ ها');
+                    }
+                })
+    }
+
+
+    useEffect(() => {
+        get_categories();
+    }, [refreshCategories]);
+
+    useEffect(() => {
+        get_brands();
+    }, [refreshBrands]);
+    useEffect(() => {
+        get_tags();
+    }, [refreshTags]);
+
+    const handleCreate = (e) => {
+        if(e)
+        {
+            let dataobj = {
+                'name':e
+            }
+            const data = Simple_get('https://hitmug.ir/api/product-category/create' , true , '',cookie.token ,'post',{...dataobj})
+                .then((d)=>{
+                    if(d?.[2]>=200 && d?.[2]<=300)
+                    {
+                        change_refresh_categories()
+                    }
+                    else
+                    {
+                        Notifier('danger' ,'خطا در ایجاد دسته بندی ها');
+                    }
+                })
+        }
+    }
+
+    const handleCreate_tag = (e) => {
+        console.log(e)
+        if(e)
+        {
+            let dataobj = {
+                'name':e
+            }
+            const data = Simple_get('https://hitmug.ir/api/tag/create' , true , '',cookie.token ,'post',{...dataobj})
+                .then((d)=>{
+                    if(d?.[2]>=200 && d?.[2]<=300)
+                    {
+                        chanage_refresh_tags()
+                    }
+                    else
+                    {
+                        Notifier('danger' ,'خطا در ایجاد تگ');
+                    }
+                })
+        }
+    }
+    const handleCreate_brand = (e) => {
+        if(e)
+        {
+            let dataobj = {
+                'name':e
+            }
+            const data = Simple_get('https://hitmug.ir/api/brand/create' , true , '',cookie.token ,'post',{...dataobj})
+                .then((d)=>{
+                    if(d?.[2]>=200 && d?.[2]<=300)
+                    {
+                        chanage_refresh_brands()
+                    }
+                    else
+                    {
+                        Notifier('danger' ,'خطا در ایجاد برند');
+                    }
+                })
+        }
+    }
+    return (
         <div className={'add-p-container'}>
             <div className="add-p-inputs">
                 <div className="inputs-row">
@@ -49,23 +184,37 @@ function AddProduct()
                     <img src={data[0].picture}/>
                     <img src={data[0].picture}/>
 
+
                 </div>
             </div>
             <div className="add-p-inputs">
                 <div className="inputs-row">
                     <div className="inputs-child">
-                        <span>نام محصول</span>
-                        <SelectSearch options={[{name:'ahad' ,value:1},{name:'reza',value:2}]} value="sv" name="language"/>
+                        <span>دسته بندی محصول</span>
+                              <CreatableSelect_single
+                                        // isClearable
+                                         Focus={change_refresh_categories}
+                                         CreateOption={handleCreate}
+                                          options={categories.map((d)=>{return {value:d.id , label:d.name}})}/>
                     </div>
                     <div className="inputs-child">
-                        <span>قیمت واحد</span>
-                        <input type={'text'}/>
+                        <span>برند</span>
+                        <CreatableSelect_single
+                             // isClearable
+                            Focus={chanage_refresh_brands}
+                            CreateOption={handleCreate_brand}
+                            options={brands?.map((d)=>{return {value:d.id , label:d.name}})}/>
                     </div>
                 </div>
                 <div className="inputs-row">
                     <div className="inputs-child">
                         <span>موجودی</span>
-                        <input type={'text'}/>
+                        <CreatableSelect_multi
+
+                            Focus={chanage_refresh_tags}
+                            CreateOption={handleCreate_tag}
+                            options={tags?.map((d)=>{return {value:d.id , label:d.name}})}/>
+                        {/*<input type={'text'}/>*/}
                     </div>
                     <div className="inputs-child">
                         <span>تخفیف</span>
@@ -76,4 +225,5 @@ function AddProduct()
         </div>
     )
 }
+
 export default AddProduct;
---
+++ mug-shop/src/components/Utils/select-box/SelectBox.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import Select from 'react-select';
+import "./selectBox.css"
+import CreatableSelect from 'react-select/creatable';
+
+ export function CreatableSelect_single({options,value,Focus,CreateOption }) {
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    return (
+        <div className="custom-select">
+            <CreatableSelect
+                onFocus={Focus}
+                onCreateOption={CreateOption}
+                isClearable
+                // defaultValue={selectedOption}
+                // onChange={setSelectedOption}
+                options={options}
+                className={"custom-select-div"}
+                placeholder={""}
+            />
+        </div>
+    );
+}
+
+
+
+ export function  CreatableSelect_multi({options,value,Focus,CreateOption  }) {
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    return (
+        <div className="custom-select">
+            <CreatableSelect
+                isMulti={true}
+                isClearable={true}
+                onFocus={Focus}
+                onCreateOption={CreateOption}
+                // defaultValue={selectedOption}
+                // onChange={setSelectedOption}
+                isRtl={true}
+                options={options}
+                className={"custom-select-div"}
+                placeholder={""}
+            />
+        </div>
+    );
+}
+
+
+// export {CreatableSelect}
